c3b699eedeeb0b547c12931525055e029b1bf3b8
IrukNuj <kurijune@gmail.com>
2018-04-10 23:36:13 +0900

    つよくてニューゲーム

---
+++ config/routes.rb
@@ -0,0 +1,8 @@
+Rails.application.routes.draw do
+  devise_for :users
+  get 'home/index'
+  get 'mahjong_course/index'
+  root 'home#index'
+  get 'mahjong_course/1'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
