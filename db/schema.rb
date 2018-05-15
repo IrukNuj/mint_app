8639d9c940b68ba54425080dc796204b7d597d92
IrukNuj <b1716568@planet.kanazawa-it.ac.jp>
2018-05-15 20:25:09 +0900

    更新

--- db/schema.rb
+++ db/schema.rb
@@ -16,18 +16,8 @@ ActiveRecord::Schema.define(version: 20180428225654) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "to_user"
-    t.string "from_user"
     t.string "from_user_id"
     t.string "post_id"
-  end
-
-  create_table "image_files", force: :cascade do |t|
-    t.string "title"
-    t.string "image"
-    t.string "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "likes", force: :cascade do |t|
@@ -66,7 +56,6 @@ ActiveRecord::Schema.define(version: 20180428225654) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "username"
-    t.string "icon"
     t.string "picture"
     t.string "avatar"
     t.index ["email"], name: "index_users_on_email", unique: true
---
+++ vendor/bundle/ruby/2.3.0/gems/ffi-1.9.23/ext/ffi_c/libffi-x86_64-linux/include/ffitarget.h
@@ -0,0 +1,1 @@
+/mnt/c/Users/Iruk/Desktop/various/Programming/Cirkit_sys/mint_app/vendor/bundle/ruby/2.3.0/gems/ffi-1.9.23/ext/ffi_c/libffi/src/x86/ffitarget.h
